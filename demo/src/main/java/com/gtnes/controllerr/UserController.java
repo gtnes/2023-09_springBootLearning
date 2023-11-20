5ad240511b41b8fefadfcc29d335bd44f6c2bd68
Gtnes <gtnes@163.com>
2023-11-20 23:00:36 +0800

    修改

---
+++ demo/src/main/java/com/gtnes/controllerr/UserController.java
@@ -0,0 +1,31 @@
+package com.gtnes.controllerr;
+
+import org.apache.catalina.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/users",method = RequestMethod.POST)
+    @ResponseBody
+    public String save(){
+        System.out.println("新增用户");
+        return "{module: '新增用户'}";
+    }
+
+    @RequestMapping(value = "/users/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public String get(@PathVariable Integer id){
+        System.out.println("获取用户信息 ID：" + id);
+        return "{module: '获取用户信息 ID: "+id+"'}";
+    }
+
+    @RequestMapping(value = "/users/{id}",method = RequestMethod.PUT)
+    @ResponseBody
+    public String update(@RequestBody User user){
+        System.out.println("修改用户信息 ID：" + user);
+        return "{module: '修改用户信息 ID:"+user+"'}";
+    }
+
+}
