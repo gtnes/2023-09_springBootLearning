8c1a25c117758cf5a8752ce8b96be9f7550d8313
Gtnes <gtnes@163.com>
2023-12-18 22:24:43 +0800

    修改

--- demo/src/main/java/com/gtnes/service/IBookService.java
+++ demo/src/main/java/com/gtnes/service/IBookService.java
@@ -5,5 +5,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.gtnes.domain.Book;
 
 public interface IBookService extends IService<Book> {
-
+    // 追加的操作与原始操作通过名称区分，功能类似
+    boolean bookSave(Book book);
+    boolean bookUpdate(Book book);
+    boolean bookDelete(Integer id);
 }
--- demo/src/main/java/com/gtnes/service/impl/BookServiceImpl.java
+++ demo/src/main/java/com/gtnes/service/impl/BookServiceImpl.java
@@ -4,9 +4,27 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gtnes.dao.BookDao;
 import com.gtnes.domain.Book;
 import com.gtnes.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl extends ServiceImpl<BookDao, Book> implements IBookService {
 
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public boolean bookSave(Book book) {
+        return bookDao.insert(book) > 0;
+    }
+
+    @Override
+    public boolean bookUpdate(Book book) {
+        return bookDao.updateById(book) > 0;
+    }
+
+    @Override
+    public boolean bookDelete(Integer id) {
+        return bookDao.deleteById(id) > 0;
+    }
 }
